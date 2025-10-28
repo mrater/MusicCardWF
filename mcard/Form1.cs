e93fa2f851c04f686fce15e222071ec4976d39a5
Maciej Sikorski <maciejosikorski@gmail.com>
2025-10-28 16:37:53 +0100

    Init

---
+++ mcard/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using SharpDX;
+using SharpDX.DirectSound;
+using SharpDX.Multimedia;
+
+namespace mcard
+{
+    public partial class Form1 : Form
+    {
+        //file fields
+        private FileInfo file;
+
+
+        // method1: DirectSound fields
+        private DirectSound _directSoundDevice;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDirectSound(this.Handle);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void selectFileButtonClick(object sender, EventArgs e)
+        {
+            //TODO: implement file selecting
+        }
+
+        
+        // methods responsible for directound
+        #region directsound
+        private void InitializeDirectSound(IntPtr hwnd)
+        {
+            _directSoundDevice = new DirectSound();
+            //IntPtr hwnd = Process.GetCurrentProcess().MainWindowHandle;
+            _directSoundDevice.SetCooperativeLevel(hwnd, CooperativeLevel.Priority);
+        }
+
+
+        private void PlayStartDirectSoundButton_Click(object sender, EventArgs e)
+        {
+            // TODO: ensure that filePath should indeed be file.Name. Note: use file object to get info
+            String filePath = file.Name;
+            if (string.IsNullOrEmpty(filePath))
+                return;
+
+            NativeMethods.PlaySound(filePath, IntPtr.Zero, NativeMethods.SoundFlags.SND_FILENAME | NativeMethods.SoundFlags.SND_ASYNC);
+        }
+
+        private void PlayStopDirectSoundButton_Click(object sender, EventArgs e)
+        {
+            NativeMethods.PlaySound(null, IntPtr.Zero, NativeMethods.SoundFlags.SND_PURGE);
+        }
+
+        #endregion
+
+        #region wmp
+        //TODO do wwmp
+        #endregion
+
+
+        #region mci
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern int waveOutOpen(out IntPtr hWaveOut, int uDeviceID, ref WaveFormat lpFormat, IntPtr dwCallback, IntPtr dwInstance, int dwFlags);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern int waveOutPrepareHeader(IntPtr hWaveOut, ref WaveHeader lpWaveOutHdr, int uSize);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern int waveOutWrite(IntPtr hWaveOut, ref WaveHeader lpWaveOutHdr, int uSize);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern int waveOutClose(IntPtr hWaveOut);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern int waveOutReset(IntPtr hWaveOut);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern int waveOutUnprepareHeader(IntPtr hWaveOut, ref WaveHeader lpWaveOutHdr, int uSize);
+
+        [DllImport("winmm.dll", CharSet = CharSet.Unicode)]
+        internal static extern int mciSendString(string lpszCommand, StringBuilder? lpszReturnString, int cchReturn, System.IntPtr hwndCallback);
+
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ mcard/NativeMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace mcard
+{
+    internal static partial class NativeMethods
+    {
+        [DllImport("winmm.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool PlaySound(string pszSound, System.IntPtr hmod, SoundFlags fdwSound);
+
+        [System.Flags]
+        internal enum SoundFlags : int
+        {
+            /// <summary>play synchronously (default)</summary>
+            SND_SYNC = 0x0000,
+            /// <summary>play asynchronously</summary>
+            SND_ASYNC = 0x0001,
+            /// <summary>silence (!default) if sound not found</summary>
+            SND_NODEFAULT = 0x0002,
+            /// <summary>pszSound points to a memory file</summary>
+            SND_MEMORY = 0x0004,
+            /// <summary>loop the sound until next sndPlaySound</summary>
+            SND_LOOP = 0x0008,
+            /// <summary>don't stop any currently playing sound</summary>
+            SND_NOSTOP = 0x0010,
+            /// <summary>Stop Playing Wave</summary>
+            SND_PURGE = 0x40,
+            /// <summary>The pszSound parameter is a filename</summary>
+            SND_FILENAME = 0x00020000,
+            /// <summary>name is a resource name or atom</summary>
+            SND_RESOURCE = 0x00040000
+        }
+    }
+}
